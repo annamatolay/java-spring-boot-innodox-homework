db9d4d4ee102b25830cb63eff4c6166c9a74c550
antMatchers <matolaypal@gmail.com>
2018-10-05 19:40:18 +0200

    create simple web layout with bootstrap

--- src/main/java/com/palmatolay/innodoxhomework/web/MainController.java
+++ src/main/java/com/palmatolay/innodoxhomework/web/MainController.java
@@ -1,15 +1,52 @@
 package com.palmatolay.innodoxhomework.web;
 
+import com.palmatolay.innodoxhomework.service.AccountService;
+import com.palmatolay.innodoxhomework.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+	private final String[] accountTableHead = new String[] {"id", "username", "email"};
+	private final String[] productTableHead = new String[] {"id", "name", "ownerId"};
+
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private ProductService productService;
+
 	@GetMapping("/")
-	public String index(){
+	public String index(@RequestParam(value = "c", required = false) String content, Map<String, Object> model){
+		final String account = "acc";
+		final String product = "prod";
+		if (content != null) {
+			switch (content) {
+				case account:
+					model.put("c", account);
+					model.put("theads", accountTableHead);
+					model.put("accounts", accountService.getAll());
+					break;
+				case product:
+					model.put("c", "acc");
+					model.put("theads", productTableHead);
+					model.put("products", productService.getAll());
+					break;
+				default:
+					model.put("c", "");
+			}
+		} else {
+			model.put("c", "");
+		}
+		System.out.println(content);
+		System.out.println(model.toString());
 		return "index";
 	}
 }
