5b35b25d8589167184098e9a2e33a1c979fe3ecd
antMatchers <matolaypal@gmail.com>
2018-10-05 18:03:17 +0200

    define models structure and DB schema

--- src/main/java/com/palmatolay/innodoxhomework/model/Account.java
+++ src/main/java/com/palmatolay/innodoxhomework/model/Account.java
@@ -1,7 +1,41 @@
 package com.palmatolay.innodoxhomework.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-public class Account {
+public class Account  {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
--- src/main/java/com/palmatolay/innodoxhomework/model/Product.java
+++ src/main/java/com/palmatolay/innodoxhomework/model/Product.java
@@ -1,7 +1,46 @@
 package com.palmatolay.innodoxhomework.model;
 
-import javax.persistence.Entity;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
 
 @Entity
 public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "OWNER_ID")
+	private Account owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Account getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Account owner) {
+		this.owner = owner;
+	}
 }
